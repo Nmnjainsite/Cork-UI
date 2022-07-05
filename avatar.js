109be29dfb584f636b6b93c91c565f0fa844d108
Naman Jain <jnaman475@gmail.com>
2022-07-05 13:32:49 +0530

    commit font family changes

--- avatar.js
+++ avatar.js
@@ -1,12 +1,12 @@
-var leftDrawerIcon = document.querySelector("#left-drawer-icon");
+// var leftDrawerIcon = document.querySelector("#left-drawer-icon");
 
-leftDrawerIcon.addEventListener("click",showLeftBar);
+// leftDrawerIcon.addEventListener("click",showLeftBar);
 
-function showLeftBar(){
-    document.querySelector(".nav-bar").style.visibility="visible";
-    document.querySelector(".nav-bar>ul").style.width="80%";
-    document.querySelector(".nav-bar>ul").style.marginTop="4rem";
-    document.querySelector(".nav-bar>ul").style.borderRight="none"
-    document.querySelector(".col-2").style.top = "70rem"
-    document.querySelector(".footer").style.visibility = "hidden"
-}
+// function showLeftBar(){
+//     document.querySelector(".nav-bar").style.visibility="visible";
+//     document.querySelector(".nav-bar>ul").style.width="80%";
+//     document.querySelector(".nav-bar>ul").style.marginTop="4rem";
+//     document.querySelector(".nav-bar>ul").style.borderRight="none"
+//     document.querySelector(".col-2").style.top = "70rem"
+//     document.querySelector(".footer").style.visibility = "hidden"
+// }
