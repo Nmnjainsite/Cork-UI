096a21c47aac8a1d3638260bb1a2f6ee0746fa6c
Naman Jain <jnaman475@gmail.com>
2022-07-10 22:44:16 +0530

    minor changes in footer

---
+++ app.js
@@ -0,0 +1,10 @@
+// const nightBtn = document.querySelector("#night");
+// const bg = document.querySelector(".background-body")
+// nightBtn.addEventListener("click",toggleDarkMode);
+
+// function toggleDarkMode(){
+//   document.querySelector(".background-body").style.backgroundColor =  "black";
+//   document.querySelector(".main-para").style.color="white";
+//   document.querySelector(".nav-bar").style.color="white"
+
+//     }
